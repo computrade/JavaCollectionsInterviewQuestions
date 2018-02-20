f5eb005b3ae6a65db52ecf86b95219a164710564
Yuval Ishay <yuval@computrade.co.il>
2018-02-21 01:41:00 +0200

    Update Linked HashMap example

--- Maps/src/computrade/collections/map/LinkedHashMapExample.java
+++ Maps/src/computrade/collections/map/LinkedHashMapExample.java
@@ -9,20 +9,13 @@ public class LinkedHashMapExample {
 	
     public static void main(String[] args) {
       
-            // Instantiate a HashMap with Integer as the key type and Student as the value type.
+            // Instantiate a LinkedHashMap with Integer as the key type and Student as the value type.
          	Map<Integer, Student> students = new LinkedHashMap<Integer, Student>();
-            // Instantiate four Students; the constructor arguments are 
-            // used to initialize Student attributes studentId and studentName
-            Student s1 = new Student(12, "Fred");
-            Student s2 = new Student(98, "Barney");
-            Student s3 = new Student(49, "Sher");
-            Student s4 = new Student(71, "Wilma");
-
-            // Insert all three Students into the HashMap.
-            students.put(s1.getStudentID(), s1);
-            students.put(s2.getStudentID(), s2);
-            students.put(s3.getStudentID(), s3);
-            students.put(s4.getStudentID(), s4);
+         	// Instantiate four Students and insert them into the LinkedHashMap. 
+    		students.put(12, new Student(12, "Fred"));
+    		students.put(98, new Student(98, "Barney"));
+    		students.put(49, new Student(49, "Sher"));
+    		students.put(71, new Student(71, "Wilma"));
           
             System.out.println("\nIterate through the LinkedHashMap to process all Entries (Id, Student):");
             for (Map.Entry<Integer,Student> entry : students.entrySet()) {
